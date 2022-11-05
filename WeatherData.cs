eb85d16e383326cfb8b7b1c44a7cb1ddc7ad7562
Hussain Silachi <102627825+silachi@users.noreply.github.com>
2022-11-05 05:56:57 -0400

    Add files via upload

---
+++ WeatherData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication2
+{
+    public class WeatherData : IWeatherData
+    {
+        private List<WeatherForecast> weatherStats = new List<WeatherForecast>
+        {
+            new WeatherForecast {Date=DateTime.Now, TemperatureC=-4, Summary="Warm" },
+            new WeatherForecast {Date=DateTime.Now, TemperatureC=-4, Summary="Chilly" },
+            new WeatherForecast {Date=DateTime.Now, TemperatureC=-4, Summary="Scorching" },
+            new WeatherForecast {Date=DateTime.Now, TemperatureC=-4, Summary="Sweltering" },
+            new WeatherForecast {Date=DateTime.Now, TemperatureC=-4, Summary="Balmy" }
+        };
+
+        public List<WeatherForecast> GetWeatherData() {
+            return weatherStats;
+        }
+    }
+}
